ba2d7ecc483e12b633fb48c13df1234b72b77526
saraswati-5806 <saraswatipanigrahi05@gmail.com>
2026-05-03 19:50:44 +0530

    Ultimate next level changes required

--- script.js
+++ script.js
@@ -46,3 +46,83 @@ form.addEventListener("submit", function(e) {
 
     form.reset();
 });
+
+
+const spinner = document.getElementById("spinner");
+const btnText = document.getElementById("btnText");
+
+const toast = document.getElementById("toast");
+
+function showToast(msg) {
+    toast.textContent = msg;
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let valid = true;
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+    const messageError = document.getElementById("messageError");
+
+    // RESET ERRORS
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+
+    // VALIDATION
+    if (name.value.trim() === "") {
+        nameError.textContent = "Name is required";
+        valid = false;
+    }
+
+    if (!email.value.includes("@")) {
+        emailError.textContent = "Enter valid email";
+        valid = false;
+    }
+
+    if (message.value.trim().length < 5) {
+        messageError.textContent = "Message too short";
+        valid = false;
+    }
+
+    if (!valid) return;
+
+    // LOADING START
+    spinner.style.display = "inline-block";
+    btnText.textContent = "Sending...";
+
+    setTimeout(() => {
+
+        // SAVE LOCALLY
+        const stored = JSON.parse(localStorage.getItem("messages")) || [];
+
+        stored.push({
+            name: name.value,
+            email: email.value,
+            message: message.value,
+            time: new Date().toLocaleString()
+        });
+
+        localStorage.setItem("messages", JSON.stringify(stored));
+
+        // RESET UI
+        spinner.style.display = "none";
+        btnText.textContent = "Send Message";
+
+        form.reset();
+
+        showToast("Message saved successfully!");
+
+    }, 1500);
+});
